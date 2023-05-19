705428ed12f7820030c51bba17cdf8d351b570ae
Eva Quinteros Gonzalez <ebi.quinterosgonzalez@gmail.com>
2023-05-19 13:41:08 -0300

    correcion 19/05

--- ProductManager2.js
+++ ProductManager2.js
@@ -2,25 +2,14 @@ const fs = require("fs");
 
 class ProductManager{
     constructor(path){
-        //inicializar con array vacio;
-        //this.path = "product.json";
         this.path = path;
-        this.products = [];
 
-        //validar
         if(!fs.existsSync(this.path)){
-            //crear
             fs.writeFileSync(this.path, "[]");
         }
     }
 
-    //validar --- codigo repetido; 
-    //null, undefined, "";
-
-
     async addProduct(title, description, price, thumbnail, code, stock){
-        //el objeto lo guardamos en una variable;
-        //cuerpo del objeto;
         let newProduct = {
             title: title,
             description: description,
@@ -29,116 +18,85 @@ class ProductManager{
             code: code,
             stock: stock
         }
-        //leer el archivo para corroborar si existe o no;
-        const readFile = await fs.promises.readFile(this.path, "utf-8");
-        //console.log(readFile);
 
-            //SI EXISTE ...
-            //lo parseamos;
-            let parse = JSON.parse(readFile);
-            //visualimos cuantos indices hay, y le sumamos ese valor al siguiente producto como id;
+            const readFile = await fs.promises.readFile(this.path, "utf-8");
+            const parse = JSON.parse(readFile);
             let id = parse.length;
-            newProduct = {id: id, ...newProduct}
-            //y finalmente, agregue a ese producto;
-            parse.push(newProduct)
-            await fs.promises.writeFile(this.path, JSON.stringify(parse));
-        /* else{
-            //agregar producto + id autoincrementable + guardarlo como array;
-            //SINO EXISTE
-            //que lo agregue y que inicie en 0;
-            //newProduct = {...this.products};
-            //no olvidar que newProduct es un objeto, por lo cual lo metemos a un array;
-            newProduct = {id: 0, ...newProduct};
-            await fs.promises.appendFile(this.path, JSON.stringify([newProduct]));
-        } */
+            newProduct = {id: id, ...newProduct};
 
-        return newProduct;
+            if((!newProduct.title) || (!newProduct.description) || (!newProduct.price) || (!newProduct.thumbnail) || (!newProduct.code) || (!newProduct.stock)){
+                console.log("Falta ingresar un valor!");
+                return {};
+            }
+
+            parse.push(newProduct);
+            await fs.promises.writeFile(this.path, JSON.stringify(parse));
+                return newProduct;
     }
 
     async getProducts(){
-        //primero, que lea el arreglo;
         const read = await fs.promises.readFile(this.path, "utf-8");
         const readParse = JSON.parse(read);
-        //console.log(read);
-        return readParse;
+            return readParse;
     }
 
     async getProductById(id){
-        //si existe...
-        //if(fs.existsSync(this.path)){
-            //lea el archivo;
             const read = await fs.promises.readFile(this.path, "utf-8");
-            //si existe el objeto;
+
             if(read){
-                //parsear, buscarlo por id y que lo devuelva
                 const readParse = JSON.parse(read);
                 const product = readParse.find(e => e.id === id);
                 return product;
             }else{
-                return undefined;
+                return "Not found";
             }
-        //}
     }
 
     async updateProduct(id, prop, value){
-        //recibe el id como parametro, y se puede modificar;
-        let read = await fs.promises.readFile(this.path, "utf-8");
+        if((prop !== id) && (value !== undefined || null || "")){
+            let read = await fs.promises.readFile(this.path, "utf-8");
             read = JSON.parse(read);
 
-        //cambia el valor;
-        /* const update = read.map(products =>{ 
-            if(products.id === id){
-                const newArray = { ...products, [prop]: value}
-                console.log(newArray);
-                return newArray;
-            }
-            return products;
-        }) */
-        const update = read.find(prod => prod.id == id);
-        if(update){
-            const newArray = { ...products, [prop]: value}
-                //console.log(newArray);
-                return newArray;
-        }
-        await fs.promises.writeFile(this.path, JSON.stringify(update));
+            const updateIndex = read.findIndex(prod => prod.id == id);
+            const toUpdate = read[updateIndex];
+                  read[updateIndex] = {... toUpdate, [prop]: value}
+
+  	        await fs.promises.writeFile(this.path, JSON.stringify(read));
+
+        }else{
+            const error = console.log("Ocurrio un error: Falta completar un campo");
+            return error, null;
+            //mantendra el valor asignado antes;
+        };
     }
 
     async deleteProduct(id){
-        //leer el archivo;
+        //corregir esto:
         let read = await fs.promises.readFile(this.path, "utf-8");
         let readParse = JSON.parse(read);
-        const deleteId = readParse.filter(e => e.id !== id);
-        return deleteId;
+            readParse = readParse.filter(e => e.id !== id);
+        //let deleteId = readParse.splice(id, 1);
+        /* let rewrite = await fs.promises.writeFile(this.path, JSON.stringify(readParse));
+            return rewrite; */
+        
     }
 
 }
 
-//const productManager = new ProductManager("product.json");
+const productManager = new ProductManager("product.json");
 
 
 /* const production = async () =>{
-const prod1 = await productManager.addProduct("Maceta", "Maceta de ceramica", 2500, "#", 10);
-const prod2 = await productManager.addProduct("Taza", "Taza de ceramica", 2000, "#", 12);
-const prod3 = await productManager.addProduct("Plato", "Plato de vidrio", 1200, "#", 2001, 20);
+/* await productManager.addProduct("Maceta", "Maceta de ceramica", 1500, "#", 2020, 5);
+await productManager.addProduct("Taza", "Taza de ceramica", 2000, "#", 12, 10);
+await productManager.addProduct("Plato", "Plato de vidrio", 1200, "#", 2001, 20); */
 
-}
-/* const allProducts = await productManager.getProducts()
-console.log(allProducts); */
-/* const idProduct = await productManager.getProductById(1);
-console.log(idProduct); */
+/* const modified = await productManager.updateProduct(0, "price", 2500);
+console.log(modified); */
+
 /* const deleteId = await productManager.deleteProduct(2);
 console.log(deleteId); */
 
-//production(); */
+//production();
 
 module.exports = ProductManager;
-
-/* productManager.addProduct("Maceta", "Maceta de ceramica", 2500, "#", 10);
-productManager.addProduct("Taza", "Taza de ceramica", 2000, "#", 12);
-//productManager.updateProduct(5, "description", "Hecho con resina");
-/* const allProducts = productManager.getProducts()
-console.log(allProducts); */
-/* const idProduct = productManager.getProductById(1);
-console.log(idProduct); */
-//const deleteId = productManager.deleteProduct(2);
-//console.log(deleteId);
--- test.js
+++ test.js
@@ -2,54 +2,39 @@ const ProductManager = require("./ProductManager2");
 const express = require("express");
 const app = express();
 
-//get /products; 
-//debe devolverlos dentro de un objeto;
-//query params --- ?limit=
-//si no recibe ningun limite, devuelve todos los productos;
-//si lo recibe, devolvera el numero de productos solicitados;
-// get /products/:pid --- req.param, el product id 
-//solo devolvera, el producto solicitado;
 
 const productManager = new ProductManager('product.json');
 
+
 app.get("/products", async (req, res) =>{
+
     const limit = req.query.limit;
-    //verificar si existe...
-    if(!limit){
-        try{
-            //que devuelva todos los productos;
-            const products = await productManager.getProducts();
-            console.log(products);
-            return res.json(products);
-        }catch(err){
-            //next(err);
-        }
-    }else{
-        try{
-            const products = await productManager.getProducts();
-            //si el limit existe, que muestre todos los productos; sino, que obtenga el los indices del array;
-            const oneElem = limit ? limit : products.length;
-            //envie el nuevo array con el metodo slice; con el indice 0 hasta el valor del limit asignado; 
-            return res.json(products.slice(0, oneElem));
-        }catch(err){
-            //next(err)
-        }
-    }
+    const products = await productManager.getProducts();
+
+    const oneElem = limit ? limit : products.length;
+        return res.status(200).json(products.slice(0, oneElem));
+
 })
+
 
 app.get("/products/:pid", async (req, res)=>{
+
     const pid = req.params.pid;
+    const products = await productManager.getProductById(parseInt(pid));
 
-    try {
-        const products = await productManager.getProductById(pid);
-        console.log(products);
-        return res.json(products);
-    } catch (error) {
-        //next(error)
+    if(products != undefined){
+        return res.status(201).json(
+        {message: "Producto con el id: ",    
+	     data: products
+        });   
+    }else{    
+        return res.status(404).json(
+        {message: "No existe el producto con el id: " + pid,    
+         data: {}
+        });
     }
-})
 
-
+});
 
 
 app.listen(8080, ()=>{
